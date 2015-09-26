0ae451a6a711148beddf7ae523ebc66c8fcbd005
Oliver <ofrendo@gmail.com>
2015-09-26 18:56:11 +0200

    Directly send back sparql results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,25 @@ package main.java;
 
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import main.java.sparql.SparqlHandler;
+
 public class Main {
 
   public static void main(String[] args) {
-
-	port(Integer.valueOf(System.getenv("PORT")));
+	int port = 5000;
+	try {
+		port = Integer.valueOf(System.getenv("PORT"));
+	}
+	catch (Exception e) {}
+	  
+	port(port);
     staticFileLocation("/public");
 
     get("/hello", (req, res) -> "Hello World");
@@ -21,6 +29,13 @@ public class Main {
         Map<String, Object> attributes = new HashMap<>();
         attributes.put("message", "Hello World!");
         return "root";
+    });
+    
+    post("/", (request, response) -> {
+    	String sparql = request.body();
+    	SparqlHandler handler = new SparqlHandler();
+    	String result = handler.doQuery(sparql);
+    	return result;
     });
 
   }
